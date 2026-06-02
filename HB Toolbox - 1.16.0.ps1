19306a3b21e1b8a26d1bd7030e0b571ae476911d
SequinAzor <sequin_@hotmail.com>
2026-06-02 11:39:43 +0000

    Update HB Toolbox - 1.16.0.ps1

--- HB Toolbox - 1.16.0.ps1
+++ HB Toolbox - 1.16.0.ps1
@@ -1,4 +1,3 @@
-# Run-MyTools.ps1
 # This fetches and runs the latest tools from GitHub without saving a file locally
  
 $url = "https://raw.githubusercontent.com/SequinAzor/GFL-AD-Script/refs/heads/Production/HB%20Toolbox.ps1"
