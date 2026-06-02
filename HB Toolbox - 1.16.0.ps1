3030cf5030f1cf5093b88663cccdec3cac2f233e
SequinAzor <sequin_@hotmail.com>
2026-06-02 09:41:07 +0000

    Update HB Toolbox - 1.16.0.ps1

--- HB Toolbox - 1.16.0.ps1
+++ HB Toolbox - 1.16.0.ps1
@@ -1,5 +1,5 @@
 # Run-MyTools.ps1
 # This fetches and runs the latest tools from GitHub without saving a file locally
  
-$url = "https://raw.githubusercontent.com/SequinAzor/gfl/refs/heads/main/HB%20Toolbox.ps1"
+$url = "https://raw.githubusercontent.com/SequinAzor/GFL-AD-Script/refs/heads/Production/HB%20Toolbox.ps1"
 Invoke-RestMethod -Uri $url | Invoke-Expression
